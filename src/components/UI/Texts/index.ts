9473816e5583d79a23f964568b5399b593b774c8
Pah <pah.dsf@gmail.com>
2020-09-23 18:01:50 -0300

    creating UI Texts

---
+++ src/components/UI/Texts/index.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+type SubtitleProps = {
+  color?: string
+}
+
+export const Title = styled.p`
+  margin: 0;
+  font-size: ${({ theme }) => theme.fontSize.large};
+  font-family: ${({ theme }) => theme.fonts.ribeye};
+  color: ${({ theme }) => theme.colors.success};
+`
+
+export const Subtitle = styled.p<SubtitleProps>`
+  margin: 0;
+  text-transform: uppercase;
+  font-size: ${({ theme }) => theme.fontSize.regular};
+  color: ${({ theme, color }) => (color ? color : theme.colors.text)};
+`
+
+export const Text = styled.p`
+  margin: 0;
+  font-size: ${({ theme }) => theme.fontSize.regular};
+  color: ${({ theme }) => theme.colors.text};
+`
