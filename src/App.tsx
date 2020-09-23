7f393792fb267cf04ab3bd33ea0a11292370aa8e
Pah <pah.dsf@gmail.com>
2020-09-23 15:46:25 -0300

    fix App store import

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './App.css'
-import Routes from './routes'
-import { Provider } from 'react-redux'
-import store from './redux'
+import React from 'react';
+import './App.css';
+import Routes from './routes';
+import { Provider } from 'react-redux';
+import store from './storage';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Routes />
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
