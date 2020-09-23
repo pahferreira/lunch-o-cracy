2ad93ef1794b213eebb79287fc0425f8ffaa14b2
Pah <pah.dsf@gmail.com>
2020-09-23 18:02:03 -0300

    creating home view static

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,51 @@
-import React, { FC } from 'react'
-import { Container } from './styled'
+import React, { FC, useContext } from 'react'
+import { Container, Image, TextWrapper, Content, ImageButton } from './styled'
+import { Subtitle, Title, Text } from 'components/UI/Texts'
+import { CardButton } from 'components/UI/Buttons'
+import { ThemeContext } from 'styled-components'
+import { Theme } from 'utils/theme'
+import TrophyPNG from 'assets/trophy.png'
+import GooglePNG from 'assets/google.png'
+import {
+  useGoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from 'react-google-login'
 
 const Home: FC = () => {
+  const theme = useContext<Theme>(ThemeContext)
+
+  const onLogin = (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline
+  ): void => {
+    console.log(response)
+  }
+  const { signIn } = useGoogleLogin({
+    onSuccess: onLogin,
+    clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID || '',
+    onFailure: (response: any) => console.log(response),
+    scope: 'profile',
+  })
+
   return (
     <Container>
-      Welcome to Homepage!
+      <Content>
+        <Subtitle color={theme.colors.primary}>
+          Last resturant selected
+        </Subtitle>
+        <Subtitle>21/09/2020</Subtitle>
+        <Image src={TrophyPNG} />
+        <Title>China Taiwan</Title>
+        <TextWrapper>
+          <Text>
+            Please login and take your friends to lunch in your favorite place!
+          </Text>
+        </TextWrapper>
+        <CardButton onClick={signIn}>
+          <ImageButton src={GooglePNG} />
+          Login with google
+        </CardButton>
+      </Content>
     </Container>
   )
 }
--- src/views/Home/styled.ts
+++ src/views/Home/styled.ts
@@ -1,3 +1,36 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Content = styled.div`
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Image = styled.img`
+  margin: 5% 0;
+  height: 140px;
+`
+
+export const ImageButton = styled.img`
+  height: 24px;
+  position: absolute;
+  left: 8%;
+`
+
+export const TextWrapper = styled.div`
+  text-align: center;
+  width: 50%;
+  min-width: 250px;
+  max-width: 300px;
+  margin: 20% 0;
+`
